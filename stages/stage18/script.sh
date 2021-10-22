fd60cb0864e11cb00997520217222229e9c2d15a
akshith6212 <akshith6212@gmail.com>
2021-10-22 13:38:33 +0530

    stage18

---
+++ stages/stage18/script.sh
@@ -0,0 +1,6 @@
+cd ../../spl
+./spl ../stages/stage18/int9.spl
+./spl ../stages/stage18/diskint.spl
+./spl ../stages/stage18/mod0.spl
+./spl ../stages/stage18/mod4.spl
+./spl ../stages/stage18/bootmod.spl
