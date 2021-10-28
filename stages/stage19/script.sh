ea0feb1d9e50185682290f5f90780b47ea8a5ed4
akshith6212 <akshith6212@gmail.com>
2021-10-28 23:05:37 +0530

    stage 19 with errors

---
+++ stages/stage19/script.sh
@@ -0,0 +1,6 @@
+cd ../../spl
+./spl ../stages/stage19/int9.spl
+./spl ../stages/stage19/mod1.spl
+./spl ../stages/stage19/mod2.spl
+./spl ../stages/stage19/bootmod.spl
+./spl ../stages/stage19/exhandler.spl
