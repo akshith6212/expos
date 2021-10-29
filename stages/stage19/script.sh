518b3a2058f11bff290a7f8cb7dde509d23f25a5
akshith6212 <akshith6212@gmail.com>
2021-10-29 10:17:57 +0530

    stage 19 without assignments

--- stages/stage19/script.sh
+++ stages/stage19/script.sh
@@ -4,3 +4,15 @@ cd ../../spl
 ./spl ../stages/stage19/mod2.spl
 ./spl ../stages/stage19/bootmod.spl
 ./spl ../stages/stage19/exhandler.spl
+
+cd ../expl
+./expl ../stages/stage19/init.expl
+
+cd ../xfs-interface
+
+./xfs-interface run ../stages/stage19/cmds
+
+cd ../xsm
+
+
+./xsm
