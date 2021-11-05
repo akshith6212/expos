d9d5c4bf373cbe55b78b20d5d9d706bfe46c9f6a
akshith6212 <akshith6212@gmail.com>
2021-11-05 14:00:49 +0530

    stage20 assgn 1

--- stages/stage20/script.sh
+++ stages/stage20/script.sh
@@ -5,6 +5,11 @@ cd ../../spl
 ./spl ../stages/stage20/bootmod.spl
 ./spl ../stages/stage20/scheduler.spl
 
+cd ../expl
+./expl ../stages/stage20/init.expl
+./expl ../stages/stage20/odd.expl
+./expl ../stages/stage20/even.expl
+
 cd ../xfs-interface
 
 ./xfs-interface run ../stages/stage20/cmds
