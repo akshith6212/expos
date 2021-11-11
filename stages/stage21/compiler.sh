5c9d169180f632c92e1d1bd30d9bbc895b698738
akshith6212 <akshith6212@gmail.com>
2021-11-11 17:04:57 +0530

    stage 21 completed

--- stages/stage21/compiler.sh
+++ stages/stage21/compiler.sh
@@ -1,8 +1,8 @@
 cd ../../expl
 
-./expl ../stages/stage21/hello.expl
+./expl ../stages/stage21/pid.expl
 
 cd ../xfs-interface
 
-./xfs-interface rm hello.xsm
-./xfs-interface load --exec ../stages/stage21/hello.xsm
+./xfs-interface rm pid.xsm
+./xfs-interface load --exec ../stages/stage21/pid.xsm
--- stages/stage21/script.sh
+++ stages/stage21/script.sh
@@ -1,3 +1,5 @@
+rm *.xsm
+
 cd ../../spl
 ./spl ../stages/stage21/int8.spl
 ./spl ../stages/stage21/int11.spl
@@ -5,11 +7,18 @@ cd ../../spl
 ./spl ../stages/stage21/mod0.spl
 ./spl ../stages/stage21/mod1.spl
 ./spl ../stages/stage21/bootmod.spl
+./spl ../stages/stage21/exhandler.spl
 
 cd ../expl
 ./expl ../stages/stage21/init.expl
+./expl ../stages/stage21/init-assgn1.expl
+./expl ../stages/stage21/fork3.expl
+./expl ../stages/stage21/pid.expl
 
 cd ../xfs-interface
+
+./xfs-interface rm fork3.xsm
+./xfs-interface load --exec ../stages/stage21/fork3.xsm
 
 ./xfs-interface run ../stages/stage21/cmds
 
